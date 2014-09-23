ed2c0b4b5facf458d2b7fb75a23c137120fe91b4
Alper Tugal <alper.tugal@gmail.com>
2014-09-23 21:33:13 +0200

    AngularJs added

---
+++ js/controllers/finderController.js
@@ -0,0 +1,45 @@
+﻿angular.module('finderApp.controllers', []).
+controller('finderController', function($scope) {
+    $scope.shopsList = [
+      {
+          Address: "Street 12, 3001 Zürich",
+          Phone: "+41 25 254 17 14",
+		  Title: "Loeb AG",
+		  LikesCount: 34,
+		  CommentCount: 23,
+		  FavoritCount: 1,
+		  State: "geöffnet",
+          Opening: "09:00 - 19:00"
+      },
+      {
+          Address: "Street 31, 3001 Basel",
+          Phone: "+41 31 222 22 23",
+		  Title: "Globus AG",
+		  LikesCount: 34,
+		  CommentCount: 23,
+		  FavoritCount: 1,
+		  State: "geöffnet",
+          Opening: "09:00 - 19:00"
+      },
+      {
+          Address: "Street 31, 3001 Basel",
+          Phone: "+41 31 222 22 23",
+		  Title: "Globus AG",
+		  LikesCount: 34,
+		  CommentCount: 23,
+		  FavoritCount: 1,
+		  State: "geöffnet",
+          Opening: "09:00 - 19:00"
+      },
+      {
+          Address: "Street 31, 3001 Basel",
+          Phone: "+41 31 222 22 23",
+		  Title: "Globus AG",
+		  LikesCount: 34,
+		  CommentCount: 23,
+		  FavoritCount: 1,
+		  State: "geöffnet",
+          Opening: "09:00 - 19:00"
+      }
+    ];
+});
